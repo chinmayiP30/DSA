27920cc79606c6306945b78793b571eab108881c
Chinmayi <chinmayip302006@gmail.com>
2026-06-29 21:01:10 +0530

    remove duplicates from sorted array

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,17 @@
-nums = [3, 2, 4]
-nums.sort()
-# nums becomes [2, 3, 4]
 
-target = 6
-
-i = 0
-j = len(nums) - 1
-
-while i < j:
-    if nums[i] + nums[j] == target:
-        print(i, j)
-        break
-    elif nums[i] + nums[j] < target:
-        i += 1
+nums=[1,1,2]
+a=0
+n=len(nums)
+k=1
+b=1
+while(b<n):
+    if nums[b]==nums[b-1]:
+        b+=1
+        continue
     else:
-        j -= 1
+        nums[a+1]=nums[b]
+        a+=1
+        k+=1
+        b+=1
+print(k)
+
