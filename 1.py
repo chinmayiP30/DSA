9f39d3489eac1d3c820fc568a08eef2ef4bb5556
Chinmayi <chinmayip302006@gmail.com>
2026-06-29 21:16:31 +0530

    moving zeros

--- 1.py
+++ 1.py
@@ -16,19 +16,27 @@
 #     else:
 #         j -= 1
 
-nums=[1,1,2]
-a=0
-n=len(nums)
-k=1
-b=1
-while(b<n):
-    if nums[b]==nums[b-1]:
-        b+=1
-        continue
-    else:
-        nums[a+1]=nums[b]
-        a+=1
-        k+=1
-        b+=1
-print(k)
+# nums=[1,1,2]
+# a=0
+# n=len(nums)
+# k=1
+# b=1
+# while(b<n):
+#     if nums[b]==nums[b-1]:
+#         b+=1
+#         continue
+#     else:
+#         nums[a+1]=nums[b]
+#         a+=1
+#         k+=1
+#         b+=1
+# print(k)
 
+
+nums = [0,1,0,3,12]
+j=0
+for i in range(len(nums)):
+    if nums[i]!=0:
+        nums[j],nums[i]=nums[i],nums[j]
+        j+=1
+print(nums)
